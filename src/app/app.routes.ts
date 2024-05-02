6060e469691b7265ee87a3d8b8b5bca1080de755
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-05-02 11:35:17 -0300

    Adicionando routerLink na navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./views/home/home.component";
+import {ApresentacaoComponent} from "./componentes/apresentacao/apresentacao.component";
+import {MoedasComponent} from "./componentes/moedas/moedas.component";
+import {ConverterComponent} from "./componentes/converter/converter.component";
+import {HistoricoComponent} from "./componentes/historico/historico.component";
 
-export const routes: Routes = [{path:'', component:HomeComponent}];
+export const routes: Routes = [
+  {path:'', redirectTo:'apresentacao', pathMatch:'full'},
+  {path:'apresentacao', component:ApresentacaoComponent},
+  {path:'moedas', component:MoedasComponent},
+  {path:'conversao', component:ConverterComponent},
+  {path:'historico', component:HistoricoComponent}
+];
