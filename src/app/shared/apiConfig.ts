5e2edc529f493e72700c73580279e7f611f33a64
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-05-13 16:33:26 -0300

    adicinado variáveis de ambiente no projeto

--- src/app/shared/apiConfig.ts
+++ src/app/shared/apiConfig.ts
@@ -1,4 +1,6 @@
+import {environment} from "../../environments/environment";
+
 const API_KEY = 'd94d8161de31f76892e6e894'
 export const apiConfig = {
-  apiURL: `https://v6.exchangerate-api.com/v6/${API_KEY}/`
+  apiURL: `${environment.apiURL}/${environment.apiKey}/`
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiKey: 'd94d8161de31f76892e6e894',
+  apiURL: 'https://v6.exchangerate-api.com/v6'
+};
