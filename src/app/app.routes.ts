211c4590adbe6d225a5ec8607c22f7d325d86414
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-05-16 13:45:25 -0300

    Página para url inválida e arquivos de ErrorComponent movidos para o diretório pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import {ApresentacaoComponent} from "./pages/apresentacao/apresentacao.component
 import {MoedasComponent} from "./pages/moedas/moedas.component";
 import {ConverterComponent} from "./pages/converter/converter.component";
 import {HistoricoComponent} from "./pages/historico/historico.component";
-import {ErrorComponentComponent} from "./components/error-component/error-component.component";
+import {ErrorComponentComponent} from "./pages/error-component/error-component.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
 export const routes: Routes = [
   {path:'', redirectTo:'apresentacao', pathMatch:'full'},
@@ -11,5 +12,7 @@ export const routes: Routes = [
   {path:'moedas', component:MoedasComponent},
   {path:'conversao', component:ConverterComponent},
   {path:'historico', component:HistoricoComponent},
-  {path:'erro', component:ErrorComponentComponent}
+  {path:'erro', component:ErrorComponentComponent},
+  {path:'not-found', component:NotFoundComponent},
+  {path: '**', redirectTo:'not-found',pathMatch:'full'}
 ];
